b2abf972143d0942f775f98346403802ab9645f0
Abi Travers <atravers4007@gmail.com>
2017-08-29 16:55:46 +0100

    Setup fizzbuzz

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,23 @@
+describe("Fizzbuzz", function() {
+
+  it("Prints numbers from 1 to 100", function(){
+
+
+   });
+
+   it("Prints fizz if number is divisible by 3", function(){
+
+
+    });
+
+    it("Prints buzz if number is divisible by 5", function(){
+
+
+    });
+
+    it("Prints fizzbuzz if number is divisible by both 3 & 5", function(){
+
+
+    });
+
+});
