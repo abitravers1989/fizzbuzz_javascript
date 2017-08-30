529dc46b12b0c934c9b2dc4a65033ad34e7b8927
Abi Travers <atravers4007@gmail.com>
2017-08-30 12:53:22 +0100

    First attempt at fizzbuzz

---
+++ src/fizzBuzz.js
@@ -0,0 +1,22 @@
+
+//similar to starting a class
+var FizzBuzz = function(){};
+
+FizzBuzz.prototype.play = funtion(number){
+    if (num %3 === 0){
+          return 'fizz'
+        }
+    else {
+      if (num %5 === 0){
+        return 'buzz'
+    }
+    else {
+      if (num %15 === 0 {
+        return 'fizzbuzz'}
+    }
+    else {
+      return number
+    }
+   }
+  }
+};
